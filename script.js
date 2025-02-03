eaa3420ac25ef67990109cb3e4816c98e025ddbf
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-03 12:23:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let randomStr = ""
+	let possibleChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for(let i = 0; i<l.length; i++){
+		let tempStr = Math.floor(Math.random(i))*possibleChars;
+		randomStr.push(tempStr);
+	}
+	return randomStr;
 }
 
 // Do not change the code below.
